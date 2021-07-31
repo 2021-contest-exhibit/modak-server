b52ff5aa93c143dacc0aa624751b275e5aa8167e
sinkyoungdeok <tlsrudejr123@nate.com>
2021-08-01 00:21:39 +0900

    [ADD] base dto 추가, gocamping api 활용 예시 추가, jpa lombok jdbc를 gradle에 추가

--- src/main/java/modak/camping/CampingApplication.java
+++ src/main/java/modak/camping/CampingApplication.java
@@ -1,13 +1,62 @@
 package modak.camping;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.json.JSONObject;
+import org.json.XML;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.print.DocFlavor;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
 
 @SpringBootApplication
 public class CampingApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(CampingApplication.class, args);
+
+		StringBuilder urlBuilder = new StringBuilder("http://api.visitkorea.or.kr/openapi/service/rest/GoCamping/basedList"); /*URL*/
+		urlBuilder.append("?" + URLEncoder.encode("ServiceKey","UTF-8") + "=m0tgvXV2adR1HjHBAzM0VCqTBT%2FlVCphcEKiQ%2BJVon%2FneZXUuMfpFlckFIuk%2BIIH4es%2F9qQJ354zBjsslknjZw%3D%3D"); /*Service Key*/
+		urlBuilder.append("&" + URLEncoder.encode("ServiceKey","UTF-8") + "=" + URLEncoder.encode("m0tgvXV2adR1HjHBAzM0VCqTBT/lVCphcEKiQ+JVon/neZXUuMfpFlckFIuk+IIH4es/9qQJ354zBjsslknjZw==", "UTF-8")); /*공공데이터포털에서 받은 인증키*/
+		urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*현재 페이지번호*/
+		urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("10", "UTF-8")); /*한 페이지 결과 수*/
+		urlBuilder.append("&" + URLEncoder.encode("MobileOS","UTF-8") + "=" + URLEncoder.encode("ETC", "UTF-8")); /*IOS(아이폰),AND(안드로이드),WIN(윈도우폰),ETC*/
+		urlBuilder.append("&" + URLEncoder.encode("MobileApp","UTF-8") + "=" + URLEncoder.encode("camping", "UTF-8")); /*서비스명=어플명*/
+		URL url = new URL(urlBuilder.toString());
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		conn.setRequestMethod("GET");
+		conn.setRequestProperty("Content-type", "application/json");
+		System.out.println("Response code: " + conn.getResponseCode());
+		BufferedReader rd;
+		if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+			rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+		} else {
+			rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+		}
+		StringBuilder sb = new StringBuilder();
+		String line;
+		while ((line = rd.readLine()) != null) {
+			sb.append(line);
+		}
+		rd.close();
+		conn.disconnect();
+		System.out.println(sb.toString());
+
+		String xml = sb.toString();
+		JSONObject jObject = XML.toJSONObject(xml);
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.enable(SerializationFeature.INDENT_OUTPUT);
+		Object json = mapper.readValue(jObject.toString(), Object.class);
+		String output = mapper.writeValueAsString(json);
+		System.out.println(output);
+
 	}
 
 }
---
+++ src/main/java/modak/camping/opendata/Base.java
@@ -0,0 +1,67 @@
+package modak.camping.opendata;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Data
+@Entity
+public class Base {
+    @Id
+    private Long id;
+    private String doNm;
+    private String mangeDivNm;
+    private String facltNm;
+    private String caravAcmpnyAt;
+    private String insrncAt;
+    private String induty;
+    private int siteMg3Vrticl;
+    private int siteBottomCl5;
+    private int siteMg3Width;
+    private int swrmCo;
+    private int glampSiteCo;
+    private int siteBottomCl3;
+    private int siteBottomCl4;
+    private int siteBottomCl1;
+    private int siteBottomCl2;
+    private String tel;
+    private int allar;
+    private String manageSttus;
+    private int sitedStnc;
+    private int indvdlCaravSiteCo;
+    private String operDeCl;
+    private int siteMg3Co;
+    private String brazierCl;
+    private int manageNmpr;
+    private int siteMg1Co;
+    private int gnrlSiteCo;
+    private int zipcode;
+    private String exprnProgrmAt;
+    private int extshrCo;
+    private int caravSiteCo;
+    private int autoSiteCo;
+    private int frprvtSandCo;
+    private int siteMg2Width;
+    private int siteMg1Width;
+    private int fireSensorCo;
+    private int contentId;
+    private String operPdCl;
+    private Double mapY;
+    private Double mapX;
+    private int wtrplCo;
+    private String modifiedtime;
+    private String sigunguNm;
+    private int siteMg1Vrticl;
+    private int frprvtWrppCo;
+    private String prmisnDe;
+    private String trlerAcmpnyAt;
+    private String addr1;
+    private String clturEventAt;
+    private String createdtime;
+    private int siteMg2Co;
+    private int toiletCo;
+    private String animalCmgCl;
+    private int siteMg2Vrticl;
+
+}
