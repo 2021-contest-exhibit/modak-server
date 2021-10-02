df0fdd79179d869966422a2e6c0f6623d18a880e
sinkyoungdeok <tlsrudejr123@nate.com>
2021-10-02 16:59:32 +0900

    [ADD] AI와의 통신을 위한 intro API 추가

--- src/main/java/modak/camping/opendata/OpendataController.java
+++ src/main/java/modak/camping/opendata/OpendataController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/opendata")
@@ -22,6 +23,18 @@ public class OpendataController {
     @GetMapping(value = "/base")
     public List getBase() {
         return opendataService.findAll("base");
+    }
+
+    @GetMapping(value = "/base/intro")
+    public List getBaseIntro() {
+        List<Base> baseList = opendataService.findAll("base");
+
+        List<BaseIntroDto> baseIntroDtoList = baseList.stream()
+                .filter(base -> !base.getIntro().equals("") && base.getIntro().length() >= 10 )
+                .map(base -> new BaseIntroDto(base.getContentId(), base.getIntro()))
+                .collect(Collectors.toList());
+
+        return baseIntroDtoList;
     }
 
     @PostMapping(value = "/image")
