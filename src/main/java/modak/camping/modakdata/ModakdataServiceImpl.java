5aa9e09a60c60bd258bd2a0a8fcc947e6fbcc3df
sinkyoungdeok <tlsrudejr123@nate.com>
2021-10-04 00:52:33 +0900

    [ADD] firestore에 camping 정보 전체 저장하는 api 완성

--- src/main/java/modak/camping/modakdata/ModakdataServiceImpl.java
+++ src/main/java/modak/camping/modakdata/ModakdataServiceImpl.java
@@ -4,7 +4,11 @@ import com.google.api.core.ApiFuture;
 import com.google.cloud.firestore.Firestore;
 import com.google.cloud.firestore.WriteResult;
 import com.google.firebase.cloud.FirestoreClient;
+import modak.camping.opendata.Base;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ModakdataServiceImpl implements ModakdataService {
@@ -18,5 +22,37 @@ public class ModakdataServiceImpl implements ModakdataService {
                 .document(camping.getContentId())
                 .set(camping);
         return apiFuture.get().getUpdateTime().toString();
+    }
+
+    @Override
+    public String saveCampings(List<Base> baseList) {
+        List<Camping> campingList = baseList.stream()
+                .map(base -> new Camping(
+                        Long.toString(base.getContentId()),
+                        base.getFacltNm(),
+                        0L,
+                        base.getAddr1() + "\t" + base.getAddr2(),
+                        base.getTel(),
+                        base.getLctCl(),
+                        base.getInduty(),
+                        base.getOperPdCl(),
+                        base.getOperDeCl(),
+                        base.getResveCl(),
+                        base.getPosblFcltyCl(),
+                        base.getSbrsCl(),
+                        base.getMapX(),
+                        base.getMapY()
+                ))
+                .collect(Collectors.toList());
+
+        campingList.stream().forEach(camping -> {
+            try {
+                saveCamping(camping);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+        return "ok";
     }
 }
