07ba380653c0c37efe6d9d3018607f019240a35b
sinkyoungdeok <tlsrudejr123@nate.com>
2021-10-09 00:51:04 +0900

    [FIX] CampingImage DTO에 데이터 일부 추가

--- src/main/java/modak/camping/modakdata/camping/CampingImage.java
+++ src/main/java/modak/camping/modakdata/camping/CampingImage.java
@@ -12,17 +12,25 @@ import javax.persistence.*;
 @NoArgsConstructor
 @Entity
 public class CampingImage {
-    @Id
+    @Id @GeneratedValue
     private Long id;
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "content_id")
     @JsonIgnore
     private Camping camping;
 
+    private String serialnum;
     private String modifiedtime;
+    private String imageUrl;
+
 
     public void assignCamping(Camping camping) {
         this.camping = camping;
     }
 
+    public CampingImage(String serialnum, String modifiedtime, String imageUrl) {
+        this.serialnum = serialnum;
+        this.modifiedtime = modifiedtime;
+        this.imageUrl = imageUrl;
+    }
 }
