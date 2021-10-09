4a20835dd2279a7a3f669f9abfad311e611b7328
sinkyoungdeok <tlsrudejr123@nate.com>
2021-10-09 21:54:40 +0900

    [UPDATE] (camping) 조회 api request를 body 에서 parameter로 변경

--- src/main/java/modak/camping/modakdata/dto/CampingSearchCondition.java
+++ src/main/java/modak/camping/modakdata/dto/CampingSearchCondition.java
@@ -1,10 +1,12 @@
 package modak.camping.modakdata.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class CampingSearchCondition {
-    private String regionContains;
-    private String operationType;
     private String environmentName;
+    private String operationType;
+    private String regionContains;
 }
