7ed493df917f73df3330d0d1b39434f78be9d1f2
sinkyoungdeok <tlsrudejr123@nate.com>
2021-10-13 22:17:35 +0900

    [UPDATE] (User) good 애그리거트와의 연관관계 매핑 및 good 애그리거트를 위한 메서드 생성

--- src/main/java/modak/camping/modakdata/user/User.java
+++ src/main/java/modak/camping/modakdata/user/User.java
@@ -5,10 +5,16 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import modak.camping.modakdata.good.Good;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Getter
 @ToString
@@ -22,7 +28,11 @@ public class User {
     private Long id;
     private String email;
 
+    @OneToMany(mappedBy = "user")
+    private Set<Good> goods = new HashSet<>();
+
     public User(String email) {
         this.email = email;
     }
+
 }
