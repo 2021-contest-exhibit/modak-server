299251e809378120abc4761cb58ac1c5c4f97e76
sinkyoungdeok <tlsrudejr123@nate.com>
2021-10-13 22:18:07 +0900

    [ADD] (Good) DTO 생성 및 Repository 추가

---
+++ src/main/java/modak/camping/modakdata/good/Good.java
@@ -0,0 +1,38 @@
+package modak.camping.modakdata.good;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import modak.camping.modakdata.camping.Camping;
+import modak.camping.modakdata.user.User;
+
+import javax.persistence.*;
+
+@Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Good {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "content_id")
+    private Camping camping;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    @JsonIgnore
+    private User user;
+
+    public Good(User user, Camping camping) {
+        this.camping = camping;
+        this.user = user;
+    }
+}
