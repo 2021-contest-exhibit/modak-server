775b27f6950266f537c1fc7603bb2c2e676605f1
sinkyoungdeok <tlsrudejr123@nate.com>
2021-10-22 22:20:56 +0900

    [ADD] (CampingSearch) 필터 여러개로 검색하는 기능에 name도 검색할 수 있도록 구성

--- src/main/java/modak/camping/modakdata/dto/request/FindCampingsRequestDto.java
+++ src/main/java/modak/camping/modakdata/dto/request/FindCampingsRequestDto.java
@@ -3,14 +3,17 @@ package modak.camping.modakdata.dto.request;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.util.List;
 
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class FindCampingsRequestDto {
     List<String> operationTypeEqual;
     List<String> regionContains;
     List<String> environmentEqual;
+    String nameContains;
 }
