40ab7cc7d219c546eb69f19630e90f3aa64a2635
sinkyoungdeok <tlsrudejr123@nate.com>
2021-10-30 01:09:15 +0900

    [ADD] Camping 조회에서 이용시설도 검색할 수 있도록 동적쿼리 기능추가

--- src/main/java/modak/camping/modakdata/camping/CampingRepositoryCustomImpl.java
+++ src/main/java/modak/camping/modakdata/camping/CampingRepositoryCustomImpl.java
@@ -10,6 +10,7 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import modak.camping.modakdata.dto.condition.CampingSearchCondition;
 import modak.camping.modakdata.dto.request.FindCampingsRequestDto;
 import modak.camping.modakdata.environment.QEnvironment;
+import modak.camping.modakdata.facility.QFacility;
 import modak.camping.modakdata.good.QGood;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.domain.Page;
@@ -24,144 +25,167 @@ import java.util.stream.Collectors;
 import static modak.camping.modakdata.camping.QCamping.*;
 import static modak.camping.modakdata.good.QGood.*;
 
-public class CampingRepositoryCustomImpl implements CampingRepositoryCustom{
+public class CampingRepositoryCustomImpl implements CampingRepositoryCustom {
 
-    private final JPAQueryFactory queryFactory;
+  private final JPAQueryFactory queryFactory;
 
-    public CampingRepositoryCustomImpl(EntityManager em) {
-        this.queryFactory = new JPAQueryFactory(em);
+  public CampingRepositoryCustomImpl(EntityManager em) {
+    this.queryFactory = new JPAQueryFactory(em);
+  }
+
+
+  @Override
+  public List<String> findCampingAddr() {
+    return queryFactory
+        .select(camping.addr)
+        .from(camping)
+        .fetch();
+  }
+
+  @Override
+  public Set<String> findCampingOperationType() {
+    return queryFactory
+        .select(camping.operationType)
+        .from(camping)
+        .groupBy(camping.operationType)
+        .fetch()
+        .stream()
+        .filter(operationType -> StringUtils.isNotEmpty(operationType))
+        .collect(Collectors.toSet());
+  }
+
+  @Override
+  public Page<Camping> findAll(CampingSearchCondition condition, Pageable pageable) {
+    QEnvironment environmentSub = new QEnvironment("environmentSub");
+
+    QueryResults<Camping> results = queryFactory
+        .selectFrom(camping)
+        .where(
+            regionContains(condition.getRegionContains()),
+            operationTypeEq(condition.getOperationType()),
+            environmentEq(condition.getEnvironmentName(), environmentSub),
+            contentIdEq(condition.getContentId()),
+            nameContains(condition.getNameContains())
+        )
+        .offset(pageable.getOffset())
+        .limit(pageable.getPageSize())
+        .orderBy(camping.contentId.asc())
+        .fetchResults();
+
+    return new PageImpl<>(results.getResults(), pageable, results.getTotal());
+  }
+
+  @Override
+  public Page<Camping> findAll(FindCampingsRequestDto findCampingsRequestDto, Pageable pageable) {
+    QEnvironment environmentSub = new QEnvironment("environmentSub");
+    QFacility facilitySub = new QFacility("facilitySub");
+
+    QueryResults<Camping> results = queryFactory.selectFrom(camping)
+        .where(
+            operationTypeEqList(findCampingsRequestDto.getOperationTypeEqual()),
+            regionContainsList(findCampingsRequestDto.getRegionContains()),
+            environmentEqList(findCampingsRequestDto.getEnvironmentEqual(), environmentSub),
+            facilityEqList(findCampingsRequestDto.getFacilityEqual(), facilitySub),
+            nameContains(findCampingsRequestDto.getNameContains())
+        )
+        .offset(pageable.getOffset())
+        .limit(pageable.getPageSize())
+        .orderBy(camping.contentId.asc())
+        .fetchResults();
+
+    return new PageImpl<>(results.getResults(), pageable, results.getTotal());
+  }
+
+  @Override
+  public Page<Camping> findAllToday(Pageable pageable) {
+    QueryResults<Camping> results = queryFactory
+        .select(camping)
+        .from(camping)
+        .leftJoin(camping.goods, good)
+        .groupBy(camping.contentId)
+        .orderBy(good.camping.count().desc(), camping.contentId.asc())
+        .offset(pageable.getOffset())
+        .limit(pageable.getPageSize())
+        .fetchResults();
+
+    return new PageImpl<>(results.getResults(), pageable, results.getTotal());
+  }
+
+  private BooleanExpression facilityEqList(List<String> facilityEualList, QFacility facilitySub) {
+    BooleanExpression res = null;
+
+    for (String facility : facilityEualList) {
+      res = res == null ? facilityEq(facility, facilitySub)
+          : res.and(facilityEq(facility, facilitySub));
     }
 
+    return res;
+  }
 
-    @Override
-    public List<String> findCampingAddr() {
-        return queryFactory
-                .select(camping.addr)
-                .from(camping)
-                .fetch();
+  private BooleanExpression environmentEqList(List<String> environmentEqualList,
+      QEnvironment environmentSub) {
+    BooleanExpression res = null;
+
+    for (String environment : environmentEqualList) {
+      res = res == null ? environmentEq(environment, environmentSub)
+          : res.or(environmentEq(environment, environmentSub));
     }
 
-    @Override
-    public Set<String> findCampingOperationType() {
-        return queryFactory
-                .select(camping.operationType)
-                .from(camping)
-                .groupBy(camping.operationType)
-                .fetch()
-                .stream()
-                .filter(operationType -> StringUtils.isNotEmpty(operationType))
-                .collect(Collectors.toSet());
+    return res;
+  }
+
+  private BooleanExpression operationTypeEqList(List<String> operationTypeEqualList) {
+    BooleanExpression res = null;
+
+    for (String operationTypeEqual : operationTypeEqualList) {
+      res = res == null ? operationTypeEq(operationTypeEqual)
+          : res.or(operationTypeEq(operationTypeEqual));
     }
 
-    @Override
-    public Page<Camping> findAll(CampingSearchCondition condition, Pageable pageable) {
-        QEnvironment environmentSub = new QEnvironment("environmentSub");
+    return res;
+  }
 
-        QueryResults<Camping> results = queryFactory
-                .selectFrom(camping)
-                .where(
-                        regionContains(condition.getRegionContains()),
-                        operationTypeEq(condition.getOperationType()),
-                        environmentEq(condition.getEnvironmentName(), environmentSub),
-                        contentIdEq(condition.getContentId()),
-                        nameContains(condition.getNameContains())
-                )
-                .offset(pageable.getOffset())
-                .limit(pageable.getPageSize())
-                .orderBy(camping.contentId.asc())
-                .fetchResults();
+  private BooleanExpression regionContainsList(List<String> regionContainsList) {
+    BooleanExpression res = null;
 
-        return new PageImpl<>(results.getResults(), pageable, results.getTotal());
+    for (String regionContains : regionContainsList) {
+      res = res == null ? regionContains(regionContains) : res.or(regionContains(regionContains));
     }
 
-    @Override
-    public Page<Camping> findAll(FindCampingsRequestDto findCampingsRequestDto, Pageable pageable) {
-        QEnvironment environmentSub = new QEnvironment("environmentSub");
+    return res;
+  }
 
+  private BooleanExpression regionContains(String region) {
+    return StringUtils.isEmpty(region) ? null : camping.addr.contains(region);
+  }
 
-        QueryResults<Camping> results = queryFactory.selectFrom(camping)
-                .where(
-                        operationTypeEqList(findCampingsRequestDto.getOperationTypeEqual()),
-                        regionContainsList(findCampingsRequestDto.getRegionContains()),
-                        environmentEqList(findCampingsRequestDto.getEnvironmentEqual(), environmentSub),
-                        nameContains(findCampingsRequestDto.getNameContains())
-                )
-                .offset(pageable.getOffset())
-                .limit(pageable.getPageSize())
-                .orderBy(camping.contentId.asc())
-                .fetchResults();
+  private BooleanExpression operationTypeEq(String operationType) {
+    return StringUtils.isEmpty(operationType) ? null : camping.operationType.eq(operationType);
+  }
 
+  private BooleanExpression contentIdEq(Long contentId) {
+    return contentId == -1L ? null : camping.contentId.eq(contentId);
+  }
 
-        return new PageImpl<>(results.getResults(), pageable, results.getTotal());
-    }
+  private BooleanExpression nameContains(String name) {
+    return StringUtils.isEmpty(name) ? null : camping.name.contains(name);
+  }
 
-    @Override
-    public Page<Camping> findAllToday(Pageable pageable) {
-        QueryResults<Camping> results = queryFactory
-                .select(camping)
-                .from(camping)
-                .leftJoin(camping.goods, good)
-                .groupBy(camping.contentId)
-                .orderBy(good.camping.count().desc(), camping.contentId.asc())
-                .offset(pageable.getOffset())
-                .limit(pageable.getPageSize())
-                .fetchResults();
+  private BooleanExpression environmentEq(String environmentName, QEnvironment environmentSub) {
+    return StringUtils.isEmpty(environmentName) ? null :
+        camping.contentId.in(
+            JPAExpressions.select(environmentSub.camping.contentId)
+                .from(environmentSub)
+                .where(environmentSub.name.eq(environmentName))
+        );
+  }
 
-        return new PageImpl<>(results.getResults(), pageable, results.getTotal());
-    }
-
-    private BooleanExpression environmentEqList(List<String> environmentEqualList, QEnvironment environmentSub) {
-        BooleanExpression res = null;
-
-        for (String environment : environmentEqualList) {
-            res = res == null ? environmentEq(environment, environmentSub) : res.or(environmentEq(environment, environmentSub));
-        }
-
-        return res;
-    }
-
-    private BooleanExpression operationTypeEqList(List<String> operationTypeEqualList) {
-        BooleanExpression res = null;
-
-        for (String operationTypeEqual : operationTypeEqualList) {
-            res = res == null ? operationTypeEq(operationTypeEqual) : res.or(operationTypeEq(operationTypeEqual));
-        }
-
-        return res;
-    }
-
-    private BooleanExpression regionContainsList(List<String> regionContainsList) {
-        BooleanExpression res = null;
-
-        for (String regionContains : regionContainsList) {
-            res = res == null ? regionContains(regionContains) : res.or(regionContains(regionContains));
-        }
-
-        return res;
-    }
-
-    private BooleanExpression regionContains(String region) {
-        return StringUtils.isEmpty(region) ? null : camping.addr.contains(region);
-    }
-
-    private BooleanExpression operationTypeEq(String operationType) {
-        return StringUtils.isEmpty(operationType) ? null : camping.operationType.eq(operationType);
-    }
-    private BooleanExpression contentIdEq(Long contentId) {
-        return contentId == -1L ? null : camping.contentId.eq(contentId);
-    }
-    private BooleanExpression nameContains(String name) {
-        return StringUtils.isEmpty(name) ? null : camping.name.contains(name);
-    }
-    private BooleanExpression environmentEq(String environmentName, QEnvironment environmentSub) {
-        return StringUtils.isEmpty(environmentName) ? null:
-                camping.contentId.in(
-                        JPAExpressions.select(environmentSub.camping.contentId)
-                                .from(environmentSub)
-                                .where(environmentSub.name.eq(environmentName )  )
-                );
-    }
-//    private JPAExpressions environmentNameEq(String environmentName) {
-//        return StringUtils.isEmpty(environmentName) ? null : environment.name.eq(environmentName);
-//    }
+  private BooleanExpression facilityEq(String facilityName, QFacility facilitySub) {
+    return StringUtils.isEmpty(facilityName) ? null :
+        camping.contentId.in(
+            JPAExpressions.select(facilitySub.camping.contentId)
+                .from(facilitySub)
+                .where(facilitySub.name.eq(facilityName))
+        );
+  }
 }
